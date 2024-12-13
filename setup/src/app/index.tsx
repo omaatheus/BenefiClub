b729e2fbc52f14fa43ee4e36922b57908e0f6e5a
Matheuss <matheusgida@outlook.com>
2024-12-13 12:55:39 -0300

    first commit

---
+++ setup/src/app/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Index() {
+    return(
+
+        <View style={styles.container}>
+            <Text>
+                Hello World
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, padding: 40, gap: 40
+    }
+})
