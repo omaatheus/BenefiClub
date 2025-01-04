a77ebe9f28415ff5514a06a7e87c87fafe4a2e62
Matheuss <matheusgida@outlook.com>
2025-01-04 16:56:36 -0300

    :sparkles: categories component

--- setup/src/app/home.tsx
+++ setup/src/app/home.tsx
@@ -1,9 +1,35 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
+
+import { api } from "@/services/api";
+import { useEffect, useState } from "react";
+import { Categories, CategoriesProps } from "@/components/categories";
 
 export default function Home(){
+
+    const [categories, setCategories] = useState<CategoriesProps>([])
+    const [category, setCategory] = useState("")
+
+    async function fetchCategories() {
+        try {
+            
+            const { data } = await api.get("/categories")
+
+            setCategories(data);
+            setCategory(data[0].id)
+
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Categorias", "Não foi possível carregar as categorias.")
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories()
+    }, [])
+
     return(
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Categories data={categories} selected={category} onSelect={setCategory}/>
         </View>
     )
 }
