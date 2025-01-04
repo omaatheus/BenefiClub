781e293b4f3ce3e45357b102ca0f65bb74044e90
Matheuss <matheusgida@outlook.com>
2025-01-04 18:00:44 -0300

    :sparkles: feat: places component

--- setup/src/app/_layout.tsx
+++ setup/src/app/_layout.tsx
@@ -1,22 +1,36 @@
 import { Stack } from "expo-router";
-import { colors } from "@/styles/theme"
+import { colors } from "@/styles/theme";
 import { Loading } from "@/components/loading";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold } from '@expo-google-fonts/rubik'
-
+import {
+  useFonts,
+  Rubik_600SemiBold,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik";
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold,
+  });
 
-    const [fontsLoaded] = useFonts({
-        Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold
-    })
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
 
-    if(!fontsLoaded) {
-        return <Loading />
-    }
-
-    return <Stack screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.gray[100] }
-    }} />
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.gray[100] },
+        }}
+      />
+    </GestureHandlerRootView>
+  );
 }
--- setup/src/app/home.tsx
+++ setup/src/app/home.tsx
@@ -3,11 +3,18 @@ import { View, Text, StyleSheet, Alert } from "react-native";
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
 import { Categories, CategoriesProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+
+type MarketsProps = PlaceProps & {
+
+}
 
 export default function Home(){
 
     const [categories, setCategories] = useState<CategoriesProps>([])
     const [category, setCategory] = useState("")
+    const [markets, setMarkets] = useState<MarketsProps[]>([])
 
     async function fetchCategories() {
         try {
@@ -23,19 +30,43 @@ export default function Home(){
         }
     }
 
+    async function fetchMarkets(){
+        try {
+
+            if(!category){
+                return 
+            }
+
+            
+            const {data} = await api.get('/markets/category/' + category) 
+
+            setMarkets(data)
+            
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Estabelecimentos", "Não foi possível carregar os estabelecimentos.")
+            
+        }
+    }
+
     useEffect(() => {
         fetchCategories()
     }, [])
+    useEffect(() => {
+        fetchMarkets()
+    }, [category])
 
     return(
         <View style={styles.container}>
             <Categories data={categories} selected={category} onSelect={setCategory}/>
+            <Places data={markets} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: "#CECECE",
     }
 })
