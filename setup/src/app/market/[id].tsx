9352124b4621d6237e52ba965db74daaa617134c
Matheuss <matheusgida@outlook.com>
2025-01-06 18:23:03 -0300

    :sparkles: feat: info component

--- setup/src/app/market/[id].tsx
+++ setup/src/app/market/[id].tsx
@@ -2,16 +2,22 @@ import { View, Text, Alert } from 'react-native'
 
 import { Cover } from '@/components/market/cover';
 
-import { router, useLocalSearchParams } from 'expo-router'
+import { router, useLocalSearchParams, Redirect } from 'expo-router'
 
 import { Loading } from '@/components/loading';
+
+import { PropsDetails, Details } from '@/components/market/details';
 
 import { api } from '@/services/api';
 import { useEffect, useState } from 'react';
 
+type DataProps = PropsDetails & {
+    cover: string
+}
+
 export default function Market() {
 
-    const [data, setData] = useState()
+    const [data, setData] = useState<DataProps>()
     const [isLoading, setIsLoading] = useState(true)
     
     const params = useLocalSearchParams<{id: string}>()
@@ -39,9 +45,15 @@ export default function Market() {
     if(isLoading){
         return <Loading />
     }
+
+    if(!data){
+        return <Redirect href="/home" />
+    }
     return(
         <View style={{flex: 1}}>
             <Cover uri={data.cover} />
+
+            <Details data={data} />
         </View>
     )
 }
